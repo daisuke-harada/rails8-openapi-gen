6bf1fc9b374c9a5027edabaa8b677bfba3469e71
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-02-19 21:29:33 +0900

    fix: openapi_action_template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # THIS FILE IS AUTO-GENERATED FROM OPENAPI. DO NOT EDIT BY HAND.
 # Source: api/resolved/openapi/openapi.yaml
-# Generated at: 2026-02-18T13:58:40Z
+# Generated at: 2026-02-19T12:28:48Z
 Rails.application.routes.draw do
   get "/up" => "health#index"
 end
